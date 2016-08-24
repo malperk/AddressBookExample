78e2611e0c7910f54f4814545612fc9fc26d4fe0
Alper KARATAS <alper@alper.com>
2016-08-24 22:56:24 +0300

    Fetching Contacts Fix

--- AddressBookExample/FetchingContacts.m
+++ AddressBookExample/FetchingContacts.m
@@ -13,7 +13,7 @@
 
 + (void)run {
     NSPredicate *predicate = [CNContact predicateForContactsMatchingName:@"alper"];
-    NSArray *keysToFetch = @[ CNContactGivenNameKey, CNContactFamilyNameKey ];
+    NSArray *keysToFetch = @[ CNContactGivenNameKey, CNContactFamilyNameKey,[CNContactFormatter descriptorForRequiredKeysForStyle:CNContactFormatterStyleFullName]];
     CNContactStore *store = [CNContactStore new];
     NSError *err;
     NSArray *contacts = [store unifiedContactsMatchingPredicate:predicate keysToFetch:keysToFetch error:&err];
@@ -24,17 +24,6 @@
             NSLog(@"%@", [CNContactFormatter stringFromContact:contact style:CNContactFormatterStyleFullName]);
         }
     }
-    
-    [CNContactFormatter descriptorForRequiredKeysForStyle:CNContactFormatterStyleFullName];
-    let contactStore = CNContactStore()
-    let predicate = CNContact.predicateForContactsMatchingName("John")
-    let foundContacts = try contactStore.unifiedContactsMatchingPredicate(predicate, keysToFetch: [CNContactFormatter.descriptorForRequiredKeysForStyle(.FullName)]
-                                                                          for contact in foundContacts {
-                                                                              print(CNContactFormatter.stringFromContact(contact, style: .FullName))
-                                                                          }[]
-    
-
-                                                                          
 }
 
 @end
